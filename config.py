21de527577c0c77842e4527e448fc95a569fe46c
Ozeliurs <ozeliurs@gmail.com>
2024-09-19 09:57:50 +0200

    feat(config): normalize GATUS_API_URL environment variable The GATUS_API_URL environment variable is now normalized by adding a default protocol if missing, removing trailing slashes, and appending '/api' if not present, to ensure a consistent and valid URL format.

--- config.py
+++ config.py
@@ -1,4 +1,13 @@
 import os
 
 GATUS_API_URL = os.environ.get("GATUS_API_URL")
+if GATUS_API_URL:
+
+    if not GATUS_API_URL.startswith(("http://", "https://")):
+        GATUS_API_URL = "http://" + GATUS_API_URL
+
+    GATUS_API_URL = GATUS_API_URL.rstrip('/')
+    if not GATUS_API_URL.endswith('/api'):
+        GATUS_API_URL += '/api'
+
 DISCORD_BOT_TOKEN = os.environ.get("DISCORD_BOT_TOKEN")
